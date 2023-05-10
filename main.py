55e5605b397a4eb8061047f83ae2184c0d1b8101
enriqueMota <abelmota@live.com>
2023-05-10 10:37:43 -0400

    changes

---
+++ main.py
@@ -0,0 +1,26 @@
+# print function
+print('Hello world!')
+
+# print function with new line
+print('Hello there!\nGeneral Kenobi')
+
+# print function with concatenation
+print('Hello'+' '+'World')
+
+# input function
+input('what is your name?')
+
+# input and print function
+print("Hello" + input("What is your name?"))
+
+# input, print and len function
+print(len(input("What is your name? ")))
+
+# variables
+name = input("What is your name?")
+lenght = len(name)
+print(len)
+
+
+# Day 1 final project link
+# https://replit.com/@enriqueMota/band-name-generator-start#main.py
